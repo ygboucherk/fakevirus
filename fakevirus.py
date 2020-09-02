c86a668e9748e61bddf6e7511399bf9baf7333e3
Yanis Boucherk <60298855+ygboucherk@users.noreply.github.com>
2020-09-02 14:52:25 +0200

    Add files via upload

---
+++ fakevirus.py
@@ -0,0 +1,35 @@
+import time
+
+waiting = 10
+print("Your PC will be self-destroyed in 10 seconds. \nDo not try anything for impeaching that, else damages will be increased !!!")
+
+def fake_mining(shares):
+	current_share = 0
+	count = 0
+	while (count < shares):
+		current_share = current_share+1
+		print("Found share", current_share, "- yay")
+		count = count + 1
+		time.sleep(10)
+
+
+time.sleep(waiting)
+print("Removing user data...")
+time.sleep(10)
+print("Done ! ")
+time.sleep(0.5)
+
+print("Mining bitcoin with your computer (an hack should be profitable !)")
+fake_mining(10)
+print("finished")
+
+time.sleep(2)
+
+
+print("But...")
+print("   /    \ ")
+print("   |-  -|")
+print("   | <> |")
+print("   \(--)/")
+print("All that was a joke ! ")
+print("This program is only a fake virus made for keeping you awareness against real threats. \nIf you believed that it was real, it is normal (we doesn't see viruses everyday). \nKeeping your softwares up to date, avoiding to download files from everywhere (github is a good source because of source code is public), and avoiding to open strange emails are good ways for avoiding to see your computer down.")
