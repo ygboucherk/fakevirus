14de14a3c642e317a1f60159f8590cec53b4f613
GNUOverYooouu <dtortoise99@gmail.com>
2021-05-24 15:02:51 -0700

    Fixed spacing and helped with spelling.

--- fakevirus.py
+++ fakevirus.py
@@ -1,7 +1,7 @@
 import time
 
 waiting = 10
-print("Your PC will be self-destroyed in 10 seconds. \nDo not try anything for impeaching that, else damages will be increased !!!")
+print("Your PC will be self-destroyed in 10 seconds. \nDo not try anything for impeaching that, else damages will be increased!!!")
 
 def fake_mining(shares):
 	current_share = 0
@@ -16,10 +16,10 @@ def fake_mining(shares):
 time.sleep(waiting)
 print("Removing user data...")
 time.sleep(10)
-print("Done ! ")
+print("Done! ")
 time.sleep(0.5)
 
-print("Mining bitcoin with your computer (an hack should be profitable !)")
+print("Mining bitcoin with your computer (a hack should be profitable!)")
 fake_mining(10)
 print("finished")
 
@@ -35,6 +35,6 @@ print("   |-  -|")
 print("   | <> |     ")
 print("   \(--)/  <=|D'OH")
 print("    |  | \n")
-print("All that was a joke ! ")
-print("This program is only a fake virus made for keeping you awareness against real threats. \n\nSorry if you believed that it was real. \n\nKeeping your softwares up to date, avoiding to download files from everywhere (github is a good source because of source code is public), and avoiding to open strange emails are good ways for avoiding to see your computer hacked.")
+print("All that was a joke! ")
+print("This program is only a fake virus made for checking your awareness against real threats. \n\nSorry if you believed that it was real. \n\nKeeping your softwares up to date, not downloading files from random sites (Github is a good source because the source code is public), and not opening strange emails are good ways to avoid getting hacked.")
 time.sleep(100)
